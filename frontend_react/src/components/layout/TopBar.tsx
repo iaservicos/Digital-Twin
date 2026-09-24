a17b85b235c5caa5ab5c370d580afd28fc2208b8
marcio-eduardo <marcioeduardo.ti@gmail.com>
2026-09-24 16:56:09 -0300

    feat: Adicionada logo positivo_tecnologia

--- frontend_react/src/components/layout/TopBar.tsx
+++ frontend_react/src/components/layout/TopBar.tsx
@@ -110,7 +110,7 @@ export default function TopBar() {
         <img 
           src="/Logo/positivo_tecnologia.png" 
           alt="Positivo Tecnologia" 
-          className="h-10 md:h-12 w-auto object-contain dark:filter-none filter brightness-0 dark:brightness-100 transition-all duration-200" 
+          className="h-12 md:h-16 w-auto object-contain dark:filter-none filter brightness-0 dark:brightness-100 transition-all duration-200" 
         />
       </div>
 
